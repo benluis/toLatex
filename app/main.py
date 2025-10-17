336a4770032e4f6c5d1dd10757d7bd720ea1f6e3
benluis <156473232+benluis@users.noreply.github.com>
2025-10-16 21:13:01 -0400

    user vercel blob for storage

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 # external
 import uvicorn
 from pathlib import Path
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from vercel_blob import put
-from pydantic import BaseModel
 import uuid
 
 app: FastAPI = FastAPI()
@@ -13,17 +12,15 @@ app: FastAPI = FastAPI()
 templates_dir: Path = Path(__file__).parent.joinpath("templates")
 templates: Jinja2Templates = Jinja2Templates(directory=templates_dir)
 
-class UploadRequest(BaseModel):
-    filename: str
-
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/api/v1/upload")
-async def upload_file(request: UploadRequest):
-    filename = f"{uuid.uuid4()}-{request.filename}"
-    blob = put(filename, b'')
+async def upload_file(file: UploadFile = File(...)):
+    filename = f"{uuid.uuid4()}-{file.filename}"
+    content = await file.read()
+    blob = put(filename, content)
     return {"url": blob['url']}
 
 
